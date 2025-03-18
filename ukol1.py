b579954f3aa8060075f73d1f2bf708e162f3415d
unknown <tomas.havlik@infis.cz>
2025-03-18 08:38:52 +0100

    first commit

---
+++ ukol1.py
@@ -0,0 +1,9 @@
+jmeno = input("Jak se jmenuješ?")
+vek = input("Kolik ti je let")
+print(f"Ahoj, jmenuji se {jmeno} a je mi {vek} let.")
+
+cislo1 = input(int("Napiš první desetinné číslo."))
+cislo2 = input(int("Napiš druhé desetinné čislo"))
+soucet = cislo1 + cislo2
+print(soucet)
+
