a5faf11942fde18542b7ed3321567f4eec4aad1a
unknown <tomas.havlik@infis.cz>
2025-03-18 15:23:32 +0100

    test more

---
+++ test.py
@@ -0,0 +1,5 @@
+vklad = input("Zadete počáteční vklad.")
+sazba = input("Zadejte roční úrokovou sazbu v procentech.")
+roky = input("Zadejte kolik let budete spořit.")
+finance = vklad/100*sazba*roky 
+print(finance)
---
+++ ukol2.py
@@ -0,0 +1,4 @@
+cislo1 = input("Napiš první desetinné číslo."))
+cislo2 = input("Napiš druhé desetinné čislo"))
+soucet = cislo1 + cislo2
+print(soucet)
